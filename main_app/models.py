5da7e779ffb591372e8323932416057e3d940547
Enoch Kim <jihok0519@gmail.com>
2022-07-06 11:39:41 -0400

    added new directories and folders

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+    name=models.CharField(max_length=100)
+    genre=models.CharField(max_length=30)
+    description=models.CharField(max_length=250)
+    price=models.IntegerField()
+
+    def __str__(self):
+        return self.name
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from .models import Cat
 
 def home(request):
-    return 
+    return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def game_index(request):
+    games = Game.objects.all()
+    return render(request, 'games/index.html', { 'games': games })
